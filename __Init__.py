73f10ab5d12fafe84ada9ae4dd544b198d213b0e
alidamar <47432563+alidamar@users.noreply.github.com>
2019-02-08 01:48:35 +0000

    Add files via upload

---
+++ __Init__.py
@@ -0,0 +1,13 @@
+from .client import LINE
+from .channel import Channel
+from .oepoll import OEPoll
+from akad.ttypes import OpType
+
+__copyright__       = 'Copyright 2018 by ali ramadhan'
+__version__         = '3.0.8'
+__license__         = 'BSD-3-Clause'
+__author__          = 'ali ramadhan'
+__author_email__    = 'alidamar04@gmail.com'
+__url__             = 'http://github.com/alekpk04/selfbot.py'
+
+__all__ = ['LINE', 'Channel', 'OEPoll', 'OpType']
